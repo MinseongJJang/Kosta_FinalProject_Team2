fba3667b010946def8952c3204b5776cf81e6654
LSY <LSY@DESKTOP-4KKQUA1>
2018-10-07 00:28:28 +0900

    성열-SQL문 업데이트

--- academyhelper/src/test/resources/academyhelper.sql
+++ academyhelper/src/test/resources/academyhelper.sql
@@ -1,3 +1,5 @@
+/*회원 테이블*/
+drop table users;
 create table users(
 	usr_id varchar2(100) primary key,
 	usr_pass varchar2(100) not null,
@@ -10,7 +12,8 @@ create table users(
 	usr_email varchar2(100) not null,
 	usr_tel varchar2(100 )not null
 )
-
+/*건의 게시물 테이블 및 시퀀스*/
+drop table suggestion_post;
 create table suggestion_post(
 	sug_no number primary key,
 	usr_id varchar2(100) not null,
@@ -19,9 +22,11 @@ create table suggestion_post(
 	sug_regdate date not null,
 	constraint suggestion_post_fk foreign key(usr_id) references users(usr_id) on delete cascade
 )
-
+drop sequence suggestion_post_seq;
 create sequence suggestion_post_seq start with 1 nocache
 
+/*권한 테이블*/
+drop table authorities;
 create table authorities(
 	authority varchar2(100) not null,
 	usr_id varchar2(100) not null,
@@ -29,6 +34,8 @@ create table authorities(
 	constraint authorites_pk primary key(authority,usr_id)
 )
 
+/*학원 관계자 테이블 및 시퀀스*/
+drop table academy;
 create table academy(
 	aca_no number primary key,
 	aca_name varchar2(100) not null,
@@ -38,8 +45,11 @@ create table academy(
 	constraint academy_fk foreign key(usr_id) references users(usr_id) on delete cascade
 )
 
+drop sequence academy_seq;
 create sequence academy_seq start with 1 nocache
 
+/*학원 관계자 테이블*/
+drop table aca_users;
 create table aca_users(
 	usr_id varchar2(100) not null,
 	busi_reg_num varchar2(100) not null,
@@ -48,6 +58,8 @@ create table aca_users(
 	constraint aca_users_pk primary key(usr_id)
 )
 
+/*학원 후기 게시물 테이블 및 시퀀스*/
+drop table aca_promo_post;
 create table aca_promo_post(
 	aca_promo_no number primary key,
 	usr_id varchar2(100) not null,
@@ -58,8 +70,11 @@ create table aca_promo_post(
 	constraint aca_promo_post_fk foreign key(usr_id) references aca_users on delete cascade
 )
 
+drop sequence aca_promo_post_seq;
 create sequence aca_promo_post_seq start with 1 nocache
 
+/*학원 후기 게시물 첨부 테이블 및 시퀀스*/
+drop table aca_promo_attach_file;
 create table aca_promo_attach_file(
 	promo_attach_no number primary key,
 	aca_promo_no number not null,
@@ -67,8 +82,11 @@ create table aca_promo_attach_file(
 	constraint aca_promo_attach_file_fk foreign key(aca_promo_no) references aca_promo_post(aca_promo_no) on delete cascade
 )
 
+drop sequence promo_attach_seq;
 create sequence promo_attach_seq start with 1 nocache
 
+/*교육과정 테이블 및 시퀀스*/
+drop table curriculum;
 create table curriculum(
 	cur_no number primary key,
 	aca_no number not null,
@@ -80,19 +98,22 @@ create table curriculum(
 	price number not null,
 	constraint curriculum_fk foreign key(aca_no) references academy(aca_no) on delete cascade
 )
-
+drop sequence cuuriculum_seq;
 create sequence curriculum_seq start with 1 nocache
 
+/*교육과정 첨부 테이블 및 시퀀스*/
 create table cur_attach_file(
 	cur_attach_no number primary key,
 	cur_no number not null,
 	cur_filepath varchar2(100) not null,
 	constraint cur_attach_file_fk foreign key(cur_no) references curriculum(cur_no) on delete cascade
 )
-
+drop sequence cur_attach_no_seq;
 create sequence cur_attach_no_seq start with 1 nocache
 
-create table aca_review(
+/*학원 후기 게시물 테이블 및 시퀀스*/
+drop table aca_review_post;
+create table aca_review_post(
 	aca_rev_no number primary key,
 	cur_no number not null,
 	aca_rev_title varchar2(100) not null,
@@ -100,21 +121,24 @@ create table aca_review(
 	aca_rev_regdate date not null,
 	aca_rev_hits number not null,
 	usr_id varchar2(100) not null,
-	constraint aca_review_ffk foreign key(cur_no) references curriculum(cur_no) on delete cascade,
-	constraint aca_review_sfk foreign key(usr_id) references users(usr_id) on delete cascade
+	constraint aca_review_post_ffk foreign key(cur_no) references curriculum(cur_no) on delete cascade,
+	constraint aca_review__post_sfk foreign key(usr_id) references users(usr_id) on delete cascade
 )
-
+drop sequence aca_rev_no_seq;
 create sequence aca_rev_no_seq start with 1 nocache
 
+/*학원 후기 게시물 첨부 테이블 및 시퀀스*/
 create table aca_rev_attach_file(
-	rev_attach_no number primary key,
+	aca_rev_attach_no number primary key,
 	aca_rev_no number not null,
 	aca_rev_filepath varchar2(100) not null,
-	constraint aca_rev_attach_file_fk foreign key(aca_rev_no) references aca_review(aca_rev_no) on delete cascade
+	constraint aca_rev_attach_file_fk foreign key(aca_rev_no) references aca_review_post(aca_rev_no) on delete cascade
 )
 
-create sequence rev_attach_no_seq start with 1 nocache
+drop sequence aca_rev_attach_no_seq;
+create sequence aca_rev_attach_no_seq start with 1 nocache
 
+/*만족도 테이블*/
 create table aca_cur_satisfaction(
 	aca_rev_no number not null,
 	cur_satis varchar2(100) not null,
@@ -122,9 +146,11 @@ create table aca_cur_satisfaction(
 	lecturer_satis varchar2(100) not null,
 	emp_links_satis varchar2(100) not null,
 	traffic_satis varchar2(100) not null,
-	constraint aca_cur_satisfaction_fk foreign key(aca_rev_no) references aca_review(aca_rev_no) on delete cascade
+	constraint aca_cur_satisfaction_fk foreign key(aca_rev_no) references aca_review_post(aca_rev_no) on delete cascade
 )
 
+/*취업 후기 게시물 테이블*/
+drop table emp_review_post;
 create table emp_review_post(
 	emp_rev_no number primary key,
 	usr_id varchar2(100) not null,
@@ -137,9 +163,16 @@ create table emp_review_post(
 	constraint emp_review_post_sfk foreign key(cur_no) references curriculum(cur_no) on delete cascade
 )
 
+drop sequence emp_rev_no_seq;
+create sequence emp_rev_no_seq start with 1 nocache
+
+/*취업 후기 게시물 첨부 테이블 및 시퀀스*/
 create table emp_review_attach_file(
 	emp_rev_attach_no number primary key,
 	emp_rev_no number not null,
 	emp_rev_filepath varchar2(100) not null,
 	constraint emp_review_attach_file_fk foreign key(emp_rev_no) references emp_review_post on delete cascade
 )
+
+drop sequence emp_rev_attach_no_seq;
+create sequence emp_rev_attach_no_seq start with 1 nocache
