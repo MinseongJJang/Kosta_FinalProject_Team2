0f99d3628dd1f9c1d58175e295d2e8783246f32e
kms <kms@192.168.0.107>
2018-10-06 15:51:52 +0900

    unit / sql

---
+++ academyhelper/src/test/java/org/kosta/academy/MemberUnit.java
@@ -0,0 +1,11 @@
+package org.kosta.academy;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="file:src/main/webapp/WEB-INF/spring-model.xml")
+public class MemberUnit {
+	//member 단위 테스트
+}
