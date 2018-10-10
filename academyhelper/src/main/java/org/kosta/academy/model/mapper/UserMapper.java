1ec3179a55bec3a24fa9131392b09f8ab68591b8
shs2810 <shs28100@naver.com>
2018-10-10 13:21:16 +0900

    -커밋1

--- academyhelper/src/main/java/org/kosta/academy/model/mapper/UserMapper.java
+++ academyhelper/src/main/java/org/kosta/academy/model/mapper/UserMapper.java
@@ -11,7 +11,16 @@ public interface UserMapper {
 	public List<AuthoritiesVO> selectAuthorityById(String id);
 	public void registerUser(UserVO userVO);
 	public void registerUser(AcaUserVO acaUserVO);
+	public void checkUser(UserVO userVO);
+	public void checkUser(AcaUserVO acaUserVO);
 	public void updateUser(UserVO userVO);
 	public void updateUser(AcaUserVO acaUserVO);
+	public void deleteUser(UserVO userVO);
+	public void deleteUser(AcaUserVO acaUserVO);
+	public void findId(UserVO userVO);
+	public void findId(AcaUserVO acaUserVO);
+	public void findPassword(UserVO userVO);
+	public void findPassword(AcaUserVO acaUserVO);
+	public void getUserInfo(String id);
 	public void registerAuthorities(AuthoritiesVO authoritiesVO);
 }
