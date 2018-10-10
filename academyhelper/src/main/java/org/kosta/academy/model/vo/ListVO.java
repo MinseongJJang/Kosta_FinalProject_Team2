fe692832fde1d06a4811be5c1c9c3dba666da452
kms <kms@192.168.0.107>
2018-10-10 17:55:16 +0900

    paging bean

---
+++ academyhelper/src/main/java/org/kosta/academy/model/vo/ListVO.java
@@ -0,0 +1,13 @@
+package org.kosta.academy.model.vo;
+
+import org.kosta.academy.model.service.PagingBean;
+/**
+ * ListVO 페이징 처리시 view에 반환할 때 ListVO에 
+ * paging bean과 해당객체 넣어 보내준다.
+ * @author kms
+ *
+ */
+public class ListVO {
+	private PagingBean pb;
+	
+}
