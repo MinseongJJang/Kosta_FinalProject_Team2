8bcd9635351517b7e4bbfedf9f1df658ce766d41
shs2810 <shs28100@naver.com>
2018-10-18 09:47:59 +0900

    test

--- academyhelper/src/main/java/org/kosta/academy/controller/FAQAndNoticeController.java
+++ academyhelper/src/main/java/org/kosta/academy/controller/FAQAndNoticeController.java
@@ -49,7 +49,7 @@ public class FAQAndNoticeController {
 		return "faq/register_faq_form.tiles";
 	}
 	@Secured("ROLE_ADMIN")
-	@RequestMapping("registerFAQ.do")
+	@PostMapping("registerFAQ.do")
 	public String registerFAQ(FAQVO faqVO) {
 		UserVO userVO=new UserVO();
 		userVO.setUsrId("admin");
